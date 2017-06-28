4fe8f9fb6f4df485a20baff87437a73d124e9245
bob.ansari <babakansari@hotmail.com>
2017-06-28 17:13:29 -0400

    Using react-native-fcm to send push notification

---
+++ index.android.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import App from "./app/App";
+
+export default class bobFirebase extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#15FC00',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('bobFirebase', () => bobFirebase);
